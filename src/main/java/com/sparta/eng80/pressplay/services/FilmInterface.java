5a992abe59c824264034434600921b4e3d8955dc
Mysters789 <mysters789@gmail.com>
2021-03-23 10:50:10 +0000

    added feature to find by genre

--- src/main/java/com/sparta/eng80/pressplay/services/FilmInterface.java
+++ src/main/java/com/sparta/eng80/pressplay/services/FilmInterface.java
@@ -6,7 +6,7 @@ public interface FilmInterface extends ServiceInterface<FilmEntity>{
 
     //find by name, category, actor, language, inventory
     Iterable<FilmEntity> findByName();
-    Iterable<FilmEntity> findByCategory();
+    Iterable<FilmEntity> findByCategory(String genre);
     Iterable<FilmEntity> findByActor();
     Iterable<FilmEntity> findByLanguage();
 
