f088ab7ae6682ebff28665201cefac8ffea5db1b
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-23 10:33:27 +0000

    Added AuthenticaionManager bean

--- src/main/java/com/sparta/eng80/pressplay/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/eng80/pressplay/security/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.sparta.eng80.pressplay.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +35,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity){
 
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception{
+        return authenticationManager();
     }
 
 }
