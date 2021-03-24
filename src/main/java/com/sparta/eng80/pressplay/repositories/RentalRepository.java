d8a3a0c2d82b71a7b006ed5aee46e05f55a23b68
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-24 10:57:19 +0000

    methods to get rentals after a given date for an individual customer or all customers

--- src/main/java/com/sparta/eng80/pressplay/repositories/RentalRepository.java
+++ src/main/java/com/sparta/eng80/pressplay/repositories/RentalRepository.java
@@ -1,11 +1,20 @@
 package com.sparta.eng80.pressplay.repositories;
 
 import com.sparta.eng80.pressplay.entities.RentalEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
 
 @Repository
 public interface RentalRepository extends CrudRepository<RentalEntity, Integer> {
 
     Iterable<RentalEntity> findRentalEntitiesByCustomer_CustomerId(int id);
+
+    @Query("SELECT r FROM RentalEntity r WHERE r.customer.customerId =? 1 AND r.inventory.rented = true AND r.returnDate <= ?2")
+    Iterable<RentalEntity> findRentalsByCustomerWhereReturnDateIsBefore(int id, Timestamp date);
+
+    @Query("SELECT r FROM RentalEntity r WHERE r.inventory.rented = true AND r.returnDate <= ?1")
+    Iterable<RentalEntity> findRentalsWhereReturnDateIsBefore(Timestamp date);
 }
