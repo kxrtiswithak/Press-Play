ecf92081cf741d0ad36dc7c328340119d2328e43
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-24 12:15:59 +0000

    fixed query

--- src/main/java/com/sparta/eng80/pressplay/repositories/RentalRepository.java
+++ src/main/java/com/sparta/eng80/pressplay/repositories/RentalRepository.java
@@ -12,7 +12,7 @@ public interface RentalRepository extends CrudRepository<RentalEntity, Integer> 
 
     Iterable<RentalEntity> findRentalEntitiesByCustomer_CustomerId(int id);
 
-    @Query(value = "SELECT * FROM rental r JOIN inventory i ON r.iventory_id == i.inventory_id WHERE r.customer.customerId =? 1 AND r.inventory.rented = true AND r.returnDate <= ?2 AND (SELECT count(*) FROM rental r2 WHERE r2.inventory_id = r.inventory_id AND r2_return_date >= r.return_date) <= 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM rental r JOIN inventory i ON r.iventory_id == i.inventory_id WHERE r.customer_id = ?1 AND i.is_rented = true AND r.return_date <= ?2 AND (SELECT count(*) FROM rental r2 WHERE r2.inventory_id = r.inventory_id AND r2_return_date >= r.return_date) <= 1", nativeQuery = true)
     Iterable<RentalEntity> findMostRecentRentalsForCustomerGroupedByInventoryIdWhereReturnDateIsBefore(int id, Timestamp date);
 
     @Query(value = "SELECT * FROM rental r JOIN inventory i ON r.iventory_id == i.inventory_id WHERE i.is_rented = true AND r.return_date <= ?1 AND (SELECT count(*) FROM rental r2 WHERE r2.inventory_id = r.inventory_id AND r2_return_date >= r.return_date) <= 1", nativeQuery = true)
