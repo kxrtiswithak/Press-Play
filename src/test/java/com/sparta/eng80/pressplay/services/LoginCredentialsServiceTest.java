8672081fae1303af5156a9e9de72216cf9ad2f44
Xavier2a2 <abanjoko@spartaglobal.com>
2021-03-25 15:58:43 +0000

    LoginCredentialsService Tests. All passed.

---
+++ src/test/java/com/sparta/eng80/pressplay/services/LoginCredentialsServiceTest.java
@@ -0,0 +1,32 @@
+package com.sparta.eng80.pressplay.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@SpringBootTest
+public class LoginCredentialsServiceTest {
+
+    @Autowired
+    LoginCredentialService loginCredentialService;
+
+    @Test
+    @Order(Integer.MIN_VALUE)
+    public void contextLoads(){
+        Assertions.assertNotNull(loginCredentialService);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "LINDA.WILLIAMS@sakilacustomer.org","BARBARA.JONES@sakilacustomer.org", "SANDRA.MARTIN@sakilacustomer.org"
+    })
+    public void loadByUsernameTest(String email){
+        UserDetails user = loginCredentialService.loadUserByUsername(email);
+        Assertions.assertNotNull(user);
+    }
+}
