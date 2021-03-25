664309b6717b9f2f9c7a6bf86dea4ca38bbeeb24
Mysters789 <mysters789@gmail.com>
2021-03-25 14:33:56 +0000

    Added code to make carousel work.

--- src/main/java/com/sparta/eng80/pressplay/controllers/MainController.java
+++ src/main/java/com/sparta/eng80/pressplay/controllers/MainController.java
@@ -1,7 +1,10 @@
 package com.sparta.eng80.pressplay.controllers;
 
 import com.sparta.eng80.pressplay.entities.CategoryEntity;
+import com.sparta.eng80.pressplay.entities.FilmEntity;
+import com.sparta.eng80.pressplay.services.FilmService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MainController {
 
+    private final FilmService filmService;
+
+    public MainController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
     @GetMapping("/")
-    public String welcome(){ return "index";}
+    public String welcome(ModelMap modelMap){
+        Iterable<FilmEntity> films = filmService.findTopNMostRentedFilms(3);
+        modelMap.addAttribute("TopFilms", films);
+        return "index";
+    }
 
     @GetMapping("/index")
     public String index() { return "index"; }
