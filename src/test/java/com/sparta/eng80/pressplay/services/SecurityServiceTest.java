739a61526570f00f52f62921bd29aa5033a9a488
WahdelMW <WWoodhouse@spartaglobal.com>
2021-03-25 11:35:37 +0000

    test staff and members are authenticated by SecurityService

--- src/test/java/com/sparta/eng80/pressplay/services/SecurityServiceTest.java
+++ src/test/java/com/sparta/eng80/pressplay/services/SecurityServiceTest.java
@@ -2,6 +2,7 @@ package com.sparta.eng80.pressplay.services;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,5 +16,14 @@ public class SecurityServiceTest {
     @Order(Integer.MIN_VALUE)
     void contextLoads() {
         Assertions.assertNotNull(securityService);
+    }
+
+    @ParameterizedTest
+    @DisplayName("check staff login works")
+    @CsvSource({"Mike.Hillyer@sakilastaff.com, password",
+            "MARY.SMITH@sakilacustomer.org, password"})
+    void validUserLogin(String username, String password) {
+        securityService.autoLogin(username, password);
+        Assertions.assertTrue(securityService.isAuthenticated());
     }
 }
