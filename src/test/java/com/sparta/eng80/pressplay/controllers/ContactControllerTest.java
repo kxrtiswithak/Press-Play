93447aac56e4727735c7f42d0351c3565db1e44c
WahdelMW <WWoodhouse@spartaglobal.com>
2021-03-26 11:37:18 +0000

    test check that 'Get in touch' is always included in contact page

--- src/test/java/com/sparta/eng80/pressplay/controllers/ContactControllerTest.java
+++ src/test/java/com/sparta/eng80/pressplay/controllers/ContactControllerTest.java
@@ -2,17 +2,31 @@ package com.sparta.eng80.pressplay.controllers;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ContactControllerTest {
 
     @Autowired
     private ContactController contactController;
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testTemplate;
+
     @Test
     void loadContext() {
         Assertions.assertNotNull(contactController);
+    }
+
+    @Test
+    void checkReturn() {
+        Assertions.assertTrue(this.testTemplate.getForObject("http://localhost:" + port + "/contact", String.class).contains("Get in touch!"));
     }
 }
