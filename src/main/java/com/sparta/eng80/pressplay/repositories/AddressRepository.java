1d770528d05b200a3f1e1148e98a793923fec9c9
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-03-27 13:59:45 +0000

    crud operations used for profile updating

--- src/main/java/com/sparta/eng80/pressplay/repositories/AddressRepository.java
+++ src/main/java/com/sparta/eng80/pressplay/repositories/AddressRepository.java
@@ -1,9 +1,15 @@
 package com.sparta.eng80.pressplay.repositories;
 
 import com.sparta.eng80.pressplay.entities.AddressEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AddressRepository extends CrudRepository<AddressEntity, Integer> {
+
+    @Query("SELECT a FROM CustomerEntity c JOIN AddressEntity a ON c.address = a JOIN CityEntity ci ON a.city = ci JOIN CountryEntity co ON ci.country = co WHERE c.customerId = ?1")
+    Optional<AddressEntity> findFullAddressByCustomerId(int id);
 }
--- src/main/java/com/sparta/eng80/pressplay/repositories/CountryRepository.java
+++ src/main/java/com/sparta/eng80/pressplay/repositories/CountryRepository.java
@@ -4,6 +4,10 @@ import com.sparta.eng80.pressplay.entities.CountryEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends CrudRepository<CountryEntity, Integer> {
+
+    Optional<CountryEntity> findCountryEntityByCountryEquals(String name);
 }
